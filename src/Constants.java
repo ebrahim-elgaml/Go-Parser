6624df4e3500008594331fb6fad9bb241397632a
Ebrahim <ebrahim.el.gaml@gmail.com>
2017-05-05 17:32:20 +0200

    fininsh file 1

--- src/Constants.java
+++ src/Constants.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
+class SingleExpression {
+	String head;
+	String body;
+	public SingleExpression(String head, String body) {
+		this.head = head;
+		this.body = body;
+	}
+}
 public class Constants {
-	static int count =-1;
-	static int cond =-1;
-	static int cond1 =-1;
-	static ArrayList<String> s= new ArrayList<String>();
-	static ArrayList<String> func= new ArrayList<String>();
-	static String case1="";
-	static int extra=0;
-	static int tCounter = 1;
-	static int tInit = -1;
+	static int tCounter = 0;
+	static Queue<SingleExpression> exQueue = new LinkedList<SingleExpression>();
 	public Constants(){
-		
 	};
-
+	
+	
+	public static void main(String [] args) { 
+		String s = "ana ebrahim elgaml";
+		System.out.println(s.replace("www", "fathy"));
+	}
 }
