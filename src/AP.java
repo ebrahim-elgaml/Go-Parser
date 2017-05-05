bf2c3eb752cbce413ce963f61eeb7f027c2186c3
Ebrahim <ebrahim.el.gaml@gmail.com>
2017-05-05 12:12:48 +0200

    initialize project

---
+++ src/AP.java
@@ -0,0 +1,46 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import java_cup.runtime.Symbol;
+
+
+public class AP {
+	public static void main(String[] args) {
+
+		String inFile = "MS3/Go1.in";
+		String outFile = "MS3/Output1.out";
+
+		if (args.length > 1) {
+			inFile = args[0];
+		}
+
+		try {
+			
+			FileReader fis = new FileReader(inFile);
+			BufferedReader bis = new BufferedReader(fis);
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+
+			@SuppressWarnings("deprecation")
+			parser parser = new parser(new Lexer(bis));
+			try {
+				Symbol res = parser.parse();
+				String val= (String) res.value;
+				System.out.println("value " +val);
+				writer.write(val);
+			} catch (Exception e) {
+				writer.write("ParseError");
+			}
+			
+			fis.close();
+			bis.close();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/Constants.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Constants {
+	static int count =-1;
+	static int cond =-1;
+	static int cond1 =-1;
+	static ArrayList<String> s= new ArrayList<String>();
+	static ArrayList<String> func= new ArrayList<String>();
+	static String case1="";
+	static int extra=0;
+	static int tCounter = 1;
+	static int tInit = -1;
+	public Constants(){
+		
+	};
+
+}
